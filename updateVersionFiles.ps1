f2c933b7e459eae0f94aa8b749715e65bd071798
David Anthoff <anthoff@berkeley.edu>
2021-04-28 12:44:03 -0700

    Version the JuliaApp

--- updateVersionFiles.ps1
+++ updateVersionFiles.ps1
@@ -15,5 +15,10 @@ $cVersionHeader | Out-File  -FilePath launcher/version.h
 [xml]$xmlDoc = Get-Content msix\Julia.appinstaller
 $xmlDoc.AppInstaller.Version = $version
 $xmlDoc.AppInstaller.MainBundle.Version = $version
-$xmlDoc.AppInstaller.MainBundle.Uri = "https://winjulia.s3-us-west-1.amazonaws.com/JuliaApp.appxbundle"
+$xmlDoc.AppInstaller.MainBundle.Uri = "https://winjulia.s3-us-west-1.amazonaws.com/JuliaApp-$($version).appxbundle"
 $xmlDoc.Save("msix\Julia.appinstaller")
+
+[xml]$xmlDoc = Get-Content msix\PackagingLayout.xml
+$xmlDoc.PackagingLayout.PackageFamily.ID = "JuliaApp-$($version)"
+$xmlDoc.PackagingLayout.PackageFamily.Package.ID = "JuliaApp-x64-$($version)"
+$xmlDoc.Save("msix\PackagingLayout.xml")
