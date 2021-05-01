4c88b35a2a9178e38b34a63cd9dce3a6b63ee02d
David Anthoff <anthoff@berkeley.edu>
2021-04-30 17:56:39 -0700

    Progress

--- launcher/generatedjuliaversions.cpp
+++ launcher/generatedjuliaversions.cpp
@@ -4,5 +4,18 @@ std::vector<JuliaVersion> JuliaVersionsDatabase::getJuliaVersions() {
 	std::vector<JuliaVersion> juliaVersions = { 
     JuliaVersion{1, 5, 1}, JuliaVersion{1, 5, 2}, JuliaVersion{1, 5, 3}, JuliaVersion{1, 5, 4}, JuliaVersion{1, 6, 0}, JuliaVersion{1, 6, 1}
 	};
+  std::sort(juliaVersions.begin(), juliaVersions.end(), [](const JuliaVersion& a, const JuliaVersion& b) {
+		if (a.major == b.major) {
+			if (a.minor == b.minor) {
+				return a.patch < b.patch;
+			}
+			else {
+				return a.minor < b.minor;
+			}
+		}
+		else {
+			return a.major < b.major;
+		}
+	});
 	return juliaVersions;
 }
