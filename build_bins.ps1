3fce0ecdb4e99979dfb50a6f8c1528f5af3b059c
David Anthoff <anthoff@berkeley.edu>
2021-05-06 13:32:26 -0700

    Use different path for build tools

--- build_bins.ps1
+++ build_bins.ps1
@@ -1,2 +1,2 @@
-&"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release /property:Platform=x86 WinJulia.sln
-&"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release /property:Platform=x64 WinJulia.sln
+&"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release /property:Platform=x86 WinJulia.sln
+&"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\MSBuild.exe" /property:Configuration=Release /property:Platform=x64 WinJulia.sln
