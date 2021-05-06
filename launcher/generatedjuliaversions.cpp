f7c4b52f2cdbe994fdda915d5bdb0fccd5a2b197
David Anthoff <anthoff@berkeley.edu>
2021-05-06 12:57:20 -0700

    Update versions

--- launcher/generatedjuliaversions.cpp
+++ launcher/generatedjuliaversions.cpp
@@ -2,7 +2,7 @@
 
 std::vector<JuliaVersion> JuliaVersionsDatabase::getJuliaVersions() {
 	std::vector<JuliaVersion> juliaVersions = { 
-    JuliaVersion{1, 5, 1}, JuliaVersion{1, 5, 2}, JuliaVersion{1, 5, 3}, JuliaVersion{1, 5, 4}, JuliaVersion{1, 6, 0}, JuliaVersion{1, 6, 1}
+    JuliaVersion{1, 5, 1}, JuliaVersion{1, 5, 2}
 	};
   std::sort(juliaVersions.begin(), juliaVersions.end(), [](const JuliaVersion& a, const JuliaVersion& b) {
 		if (a.major == b.major) {
