1d077edce14177dc48420b4aba7c95842903d0d7
David Anthoff <anthoff@berkeley.edu>
2021-05-12 12:37:58 -0700

    Remove signing of Julia binaries

--- build_optional_packages.ps1
+++ build_optional_packages.ps1
@@ -9,19 +9,7 @@ $versions = Get-Content versions.json | ConvertFrom-Json
 $versions.OptionalJuliaPackages | ForEach-Object -Parallel {
     $juliaVersion = $_.JuliaVersion
     $version = $_.Version
-
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win64\julia-$juliaVersion\bin\*.exe"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win64\julia-$juliaVersion\bin\*.dll"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win64\julia-$juliaVersion\libexec\*.exe"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win64\julia-$juliaVersion\libexec\*.dll"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win64\julia-$juliaVersion\lib\julia\*.dll"
-
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win32\julia-$juliaVersion\bin\*.exe"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win32\julia-$juliaVersion\bin\*.dll"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win32\julia-$juliaVersion\libexec\*.exe"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win32\julia-$juliaVersion\libexec\*.dll"
-    &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 467A053795F772FC96BC766AD85D2C039E4DF9B3 "optionalpackages\win32\julia-$juliaVersion\lib\julia\*.dll"
-
+    
     push-location msix
     &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\MakeAppx.exe" build /o /id "Julia-$juliaVersion-$version" /f PackagingLayoutOptionalPackages.xml /op ..\output\optional /pv $version /bv $version /bc
     &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\MakeAppx.exe" build /o /id "Julia-x86-$juliaVersion-$version" /f PackagingLayoutOptionalPackages.xml /op ..\output\optional /pv $version /bv $version /bc
