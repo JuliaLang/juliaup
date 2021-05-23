775edf5ca311dc5ceee9b0f06cfc21272910696b
David Anthoff <anthoff@berkeley.edu>
2021-05-23 18:08:45 +0200

    Disable sign step

--- build_msix.ps1
+++ build_msix.ps1
@@ -9,6 +9,6 @@ push-location msix
 &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\MakeAppx.exe" build /f PackagingLayout.xml /op ..\build\msix /pv $version /bv $version
 pop-location
 
-&"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 E70A5E7F058A0E4FCAAC9CC604C44EC8588D1C59 build\msix\*
+# &"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool" sign /fd SHA256 /sha1 E70A5E7F058A0E4FCAAC9CC604C44EC8588D1C59 build\msix\*
 
 Move-Item .\build\msix\* .\output
