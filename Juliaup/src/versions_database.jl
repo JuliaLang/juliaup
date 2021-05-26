90f9456a63ff5307170145080b6e1cf75cc47335
David Anthoff <anthoff@berkeley.edu>
2021-05-26 17:30:50 +0200

    Update version to v1.0.17.0

--- Juliaup/src/versions_database.jl
+++ Juliaup/src/versions_database.jl
@@ -1,4 +1,4 @@
-JULIA_APP_VERSION = v"1.0.16"
+JULIA_APP_VERSION = v"1.0.17"
 
 JULIA_VERSIONS = [
   VersionNumber(1, 5, 1), VersionNumber(1, 5, 2), VersionNumber(1, 5, 3), VersionNumber(1, 5, 4), VersionNumber(1, 6, 0), VersionNumber(1, 6, 1)
