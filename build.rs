5fe7699eb59ef7fedbb9fc4847a05636a39e7e16
David Anthoff <anthoff@berkeley.edu>
2021-08-09 22:57:32 -0700

    Add icon

--- build.rs
+++ build.rs
@@ -1,14 +1,25 @@
+extern crate winres;
+
+use anyhow::{Context, Result};
 use std::env;
-use std::path::PathBuf;
 use std::path::Path;
-use anyhow::{Context,Result};
+use std::path::PathBuf;
 
 fn main() -> Result<()> {
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    std::fs::copy(Path::new("build/versiondb/juliaup-versionsdb-winnt-x64.json"), out_path.join("versionsdb.json"))
-        .context("Failed to copy version DB.")?;
+    std::fs::copy(
+        Path::new("build/versiondb/juliaup-versionsdb-winnt-x64.json"),
+        out_path.join("versionsdb.json"),
+    )
+    .context("Failed to copy version DB.")?;
+
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("src/julia.ico");
+        res.compile().unwrap();
+    }
 
     Ok(())
 }
