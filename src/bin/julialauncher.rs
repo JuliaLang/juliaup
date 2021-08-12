60ccca7a7d2df94765c07a85da9115dbe2f37804
David Anthoff <anthoff@berkeley.edu>
2021-08-11 21:23:27 -0700

    Add error messages to launcher

--- src/bin/julialauncher.rs
+++ src/bin/julialauncher.rs
@@ -1,13 +1,17 @@
 use anyhow::{anyhow, Context, Result};
-use juliaup::config_file::{
-    load_config_db, JuliaupConfig, JuliaupConfigChannel
-};
+use juliaup::config_file::{load_config_db, JuliaupConfig, JuliaupConfigChannel};
+use juliaup::jsonstructs_versionsdb::JuliaupVersionDB;
 use juliaup::utils::get_juliaupconfig_path;
 use juliaup::versions_file::load_versions_db;
-use juliaup::jsonstructs_versionsdb::JuliaupVersionDB;
 use normpath::PathExt;
 use std::path::Path;
 use std::path::PathBuf;
+
+#[derive(thiserror::Error, Debug)]
+pub enum JuliaupInvalidChannel {
+    #[error("invalid channel specified")]
+    FromCmdLine(),
+}
 
 #[cfg(target_os = "windows")]
 mod bindings {
@@ -15,7 +19,10 @@ mod bindings {
 }
 
 #[cfg(target_os = "windows")]
-use bindings::Windows::Win32::{System::Console::{GetConsoleMode,GetStdHandle,SetConsoleMode,CONSOLE_MODE,ENABLE_VIRTUAL_TERMINAL_PROCESSING,STD_OUTPUT_HANDLE}};
+use bindings::Windows::Win32::System::Console::{
+    GetConsoleMode, GetStdHandle, SetConsoleMode, CONSOLE_MODE, ENABLE_VIRTUAL_TERMINAL_PROCESSING,
+    STD_OUTPUT_HANDLE,
+};
 
 #[cfg(target_os = "windows")]
 fn windows_enable_virtual_terminal_processing() -> Result<()> {
@@ -27,11 +34,14 @@ fn windows_enable_virtual_terminal_processing() -> Result<()> {
         }
 
         let mut console_mode = CONSOLE_MODE::from(0);
-        GetConsoleMode(console_handle, &mut console_mode as *mut _ as _).ok()?;
+        GetConsoleMode(console_handle, &mut console_mode as *mut _ as _)
+            .ok()
+            .with_context(|| "The call to GetConsoleMode failed.")?;
 
         console_mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-        SetConsoleMode(console_handle, console_mode).ok()?;
-    
+        SetConsoleMode(console_handle, console_mode)
+            .ok()
+            .with_context(|| "Teh call to SetConsoleMode failed")?;
         Ok(())
     }
 }
@@ -43,13 +53,18 @@ fn windows_enable_virtual_terminal_processing() -> Result<()> {
 
 fn do_initial_setup(juliaupconfig_path: &Path) -> Result<()> {
     if !juliaupconfig_path.exists() {
-        let my_own_path = std::env::current_exe()?;
+        let my_own_path = std::env::current_exe()
+            .with_context(|| "std::env::current_exe() did not find its own path.")?;
 
-        std::process::Command::new(my_own_path.parent().unwrap().join(format!("juliaup{}", std::env::consts::EXE_SUFFIX)))
+        let juliaup_path = my_own_path
+            .parent()
+            .unwrap() // unwrap OK here because this can't happen
+            .join(format!("juliaup{}", std::env::consts::EXE_SUFFIX));
+
+        std::process::Command::new(juliaup_path)
             .arg("46029ef5-0b73-4a71-bff3-d0d05de42aac") // This is our internal command to do the initial setup
-            .status()?;
-    
-    //     }
+            .status()
+            .with_context(|| "Failed to start juliaup for the initial setup.")?;
     }
     Ok(())
 }
@@ -59,8 +74,16 @@ fn check_channel_uptodate(
     current_version: &str,
     versions_db: &JuliaupVersionDB,
 ) -> Result<()> {
-    let latest_version = &versions_db.available_channels.get(channel).unwrap().version;
-    // return Err(anyhow!("The configured channel `{}` does not exist in the versions database.", channel));
+    let latest_version = &versions_db
+        .available_channels
+        .get(channel)
+        .ok_or_else(|| {
+            anyhow!(
+                "The channel `{}` does not exist in the versions database.",
+                channel
+            )
+        })?
+        .version;
 
     if latest_version != current_version {
         println!("The latest version of Julia in the `{}` channel is {}. You currently have `{}` installed. Run:", channel, latest_version, current_version);
@@ -80,79 +103,108 @@ fn get_julia_path_from_channel(
     config_data: &JuliaupConfig,
     channel: &str,
     juliaupconfig_path: &Path,
+    julia_version_from_cmd_line: bool,
 ) -> Result<(PathBuf, Vec<String>)> {
-    let channel_info = config_data.installed_channels.get(channel).unwrap(); // TODO Proper error throwing here.
-    // {
-    // 	if channel_is_from_commandline {
-    //         return Err(anyhow!("No channel with name `{}` exists in the juliaup configuration file.", channel));
-    // 	} else {
-    //         return Err(anyhow!("No channel named `{}` exists. Please use the name of an installed channel.", channel));
-    // 	}
-    // }
+    let channel_info = if julia_version_from_cmd_line {
+        config_data
+            .installed_channels
+            .get(channel)
+            .ok_or_else(|| JuliaupInvalidChannel::FromCmdLine {})?
+    } else {
+        config_data.installed_channels.get(channel)
+            .ok_or_else(|| anyhow!("The juliaup configuration is in an inconsistent state, the currently configured default channel `{}` is not installed.", channel))?
+    };
 
     match channel_info {
         JuliaupConfigChannel::LinkedChannel { command, args } => {
-            return Ok((PathBuf::from(command), args.clone().unwrap()))
+            return Ok((
+                PathBuf::from(command),
+                args.as_ref().map_or_else(|| Vec::new(), |v| v.clone()),
+            ))
         }
         JuliaupConfigChannel::SystemChannel { version } => {
-            let path = &config_data.installed_versions.get(version).unwrap().path;
-            //     throw JuliaupConfigError("The channel `" + channel + "` points to a Julia version that is not installed.");
-            check_channel_uptodate(channel, version, versions_db)?;
+            let path = &config_data
+                .installed_versions.get(version)
+                .ok_or_else(|| anyhow!("The juliaup configuration is in an inconsistent state, the channel {} is pointing to Julia version {}, which is not installed.", channel, version))?.path;
 
+            check_channel_uptodate(channel, version, versions_db).with_context(|| {
+                format!(
+                    "The Julia launcher failed while checking whether the channe {} is up-to-date.",
+                    channel
+                )
+            })?;
             let absolute_path = juliaupconfig_path
                 .parent()
-                .unwrap()
+                .unwrap() // unwrap OK because there should always be a parent
                 .join(path)
                 .join("bin")
                 .join(format!("julia{}", std::env::consts::EXE_SUFFIX))
-                .normalize()?;
-            // return normalizedPath;
+                .normalize()
+                .with_context(|| {
+                    format!(
+                        "Failed to normalize path for Julia binary, starting from `{}`.",
+                        juliaupconfig_path.display()
+                    )
+                })?;
             return Ok((absolute_path.into_path_buf(), Vec::new()));
         }
     }
 }
 
-fn main() -> Result<()> {
+fn run_app() -> Result<i32> {
     if cfg!(windows) {
-        windows_enable_virtual_terminal_processing()?;
+        windows_enable_virtual_terminal_processing()
+            .with_context(|| "The Julia launcher failed failed to configure the terminal to use ENABLE_VIRTUAL_TERMINAL_PROCESSING.")?;
     }
 
     // Set console title
+    // TODO detect whether we are in a tty or not
     println!("\x1b]2;Julia\x07");
 
-    let juliaupconfig_path = get_juliaupconfig_path()?;
+    let juliaupconfig_path = get_juliaupconfig_path()
+        .with_context(|| "The Julia launcher failed to find the juliaup configuration path.")?;
 
-    do_initial_setup(&juliaupconfig_path)?;
+    do_initial_setup(&juliaupconfig_path)
+        .with_context(|| "The Julia launcher failed to run the initial setup steps.")?;
 
-    let config_data =
-        load_config_db().with_context(|| "`status` command failed to load configuration file.")?;
+    let config_data = load_config_db()
+        .with_context(|| "The Julia launcher failed to load a configuration file.")?;
 
     let versiondb_data =
-        load_versions_db().with_context(|| "`status` command failed to load versions db.")?;
+        load_versions_db().with_context(|| "The Julia launcher failed to load a versions db.")?;
 
     let mut julia_channel_to_use = config_data.default.clone();
 
     let args: Vec<String> = std::env::args().collect();
 
-    // let mut julia_version_from_cmd_line = false;
+    let mut julia_version_from_cmd_line = false;
 
     if args.len() > 1 {
         let first_arg = &args[1];
 
         if first_arg.starts_with("+") {
             julia_channel_to_use = Some(first_arg[1..].to_string());
-            // julia_version_from_cmd_line = true;
+            julia_version_from_cmd_line = true;
         }
     }
 
-    let julia_channel_to_use = julia_channel_to_use.unwrap(); // TODO Add helpful error message
+    let julia_channel_to_use = julia_channel_to_use.ok_or_else(|| {
+        anyhow!("The Julia launcher failed to figure out which juliaup channel to use.")
+    })?;
 
     let (julia_path, julia_args) = get_julia_path_from_channel(
         &versiondb_data,
         &config_data,
         &julia_channel_to_use,
         &juliaupconfig_path,
-    )?;
+        julia_version_from_cmd_line,
+    )
+    .with_context(|| {
+        format!(
+            "The Julia launcher failed to determine the command for the `{}` channel.",
+            julia_channel_to_use
+        )
+    })?;
 
     let mut new_args: Vec<String> = Vec::new();
 
@@ -166,9 +218,21 @@ fn main() -> Result<()> {
         }
     }
 
-    std::process::Command::new(julia_path)
+    let status = std::process::Command::new(julia_path)
         .args(&new_args)
-        .status()?;
+        .status()
+        .with_context(|| "The Julia launcher failed to start Julia.")?; // TODO Maybe include the command we actually tried to start?
 
-    Ok(())
+    let code = match status.code() {
+        Some(code) => code,
+        None => 1, // TODO Is this the right call here? We get that if the sub process was terminated by a signal...
+    };
+
+    Ok(code)
+}
+
+fn main() -> Result<()> {
+    let client_status = run_app()?;
+
+    std::process::exit(client_status);
 }
