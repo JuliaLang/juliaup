58f696f90eafaa346a4449b831c52580f26b393b
David Anthoff <anthoff@berkeley.edu>
2021-08-16 10:20:27 -0700

    Disable reading of outdated versions db

--- src/versions_file.rs
+++ src/versions_file.rs
@@ -7,22 +7,23 @@ use crate::jsonstructs_versionsdb::JuliaupVersionDB;
 pub fn load_versions_db() -> Result<JuliaupVersionDB> {    
     let vendored_db = include_str!(concat!(env!("OUT_DIR"), "/versionsdb.json"));
 
-    let version_db_path =
-        get_juliaup_home_path()
-            .with_context(|| "Failed to determine versions db file path.")?
-            .join("juliaup-versionsdb-winnt-x64.json");
+    // TODO Reenable once we have an automatic way to update this file
+    // let version_db_path =
+    //     get_juliaup_home_path()
+    //         .with_context(|| "Failed to determine versions db file path.")?
+    //         .join("juliaup-versionsdb-winnt-x64.json");
 
 
-    let file = File::open(&version_db_path);
+    // let file = File::open(&version_db_path);
     
-    if let Ok(file) = file {
-        let reader = BufReader::new(file);
+    // if let Ok(file) = file {
+    //     let reader = BufReader::new(file);
     
-        let db: JuliaupVersionDB = serde_json::from_reader(reader)
-            .with_context(|| format!("Failed to parse version db at '{}'.", version_db_path.display()))?;
+    //     let db: JuliaupVersionDB = serde_json::from_reader(reader)
+    //         .with_context(|| format!("Failed to parse version db at '{}'.", version_db_path.display()))?;
 
-        return Ok(db);
-    }
+    //     return Ok(db);
+    // }
 
     let db: JuliaupVersionDB = serde_json::from_str(&vendored_db)
         .with_context(|| "Failed to parse vendored version db.")?;
