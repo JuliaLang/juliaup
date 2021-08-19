3d1fcd8a9e9ecc86f79a448702f1b755f94644bc
KristofferC <kcarlsson89@gmail.com>
2021-08-19 08:55:48 +0200

    fix command names used in errors for link command

--- src/command_link.rs
+++ src/command_link.rs
@@ -5,10 +5,10 @@ use crate::config_file::JuliaupConfigChannel;
 
 pub fn run_command_link(channel: String, file: String, args: Vec<String>) -> Result<()> {
     let mut config_data = load_config_db()
-        .with_context(|| "`status` command failed to load configuration file.")?;
+        .with_context(|| "`link` command failed to load configuration file.")?;
 
     let versiondb_data = load_versions_db()
-        .with_context(|| "`status` command failed to load versions db.")?;
+        .with_context(|| "`link` command failed to load versions db.")?;
 
     if config_data.installed_channels.contains_key(&channel) {
         bail!("Channel name `{}` is already used.", channel)
