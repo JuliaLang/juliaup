ec166d12b0f6130ad0c43d9341a372e1acf61574
David Anthoff <anthoff@berkeley.edu>
2021-11-27 11:16:45 -0800

    Add two more tests

---
+++ tests/command_add.rs
@@ -0,0 +1,25 @@
+use assert_cmd::Command;
+
+#[test]
+fn command_add() {
+    let depot_dir = assert_fs::TempDir::new().unwrap();
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("add")
+        .arg("1.6.4")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout("");
+
+    Command::cargo_bin("julialauncher")
+        .unwrap()
+        .arg("+1.6.4")
+        .arg("-e")
+        .arg("print(VERSION)")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout("1.6.4");
+}
