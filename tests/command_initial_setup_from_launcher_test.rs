e019e8de0bfe3030c66dd467f2539fa8f88eefd3
David Anthoff <anthoff@berkeley.edu>
2021-11-27 11:16:42 -0800

    Simplify tests

--- tests/command_initial_setup_from_launcher_test.rs
+++ tests/command_initial_setup_from_launcher_test.rs
@@ -5,18 +5,17 @@ use predicates::prelude::*;
 
 #[test]
 fn command_initial_setup() {
-    let mut cmd = Command::cargo_bin("juliaup").unwrap();
-
     let depot_dir = assert_fs::TempDir::new().unwrap();
 
     depot_dir.child(Path::new("juliaup")).assert(predicate::path::missing());
 
-    let assert = cmd
+    Command::cargo_bin("juliaup")
+        .unwrap()
         .arg("46029ef5-0b73-4a71-bff3-d0d05de42aac")
         .env("JULIA_DEPOT_PATH", depot_dir.path())
-        .assert();
-    
-    assert.success().stdout("");
+        .assert()
+        .success()
+        .stdout("");
 
     depot_dir.child(Path::new("juliaup").join("juliaup.json")).assert(predicate::path::exists());
 }
--- tests/command_status_test.rs
+++ tests/command_status_test.rs
@@ -2,14 +2,21 @@ use assert_cmd::Command;
 
 #[test]
 fn command_status() {
-    let mut cmd = Command::cargo_bin("juliaup").unwrap();
-
     let depot_dir = tempfile::Builder::new().prefix("juliauptest").tempdir().unwrap();
 
-    let assert = cmd
+    Command::cargo_bin("juliaup")
+        .unwrap()        
         .arg("status")
         .env("JULIA_DEPOT_PATH", depot_dir.path())
-        .assert();
-    
-    assert.success().stdout("Installed Julia channels (default marked with *):\n");
+        .assert()
+        .success()
+        .stdout("Installed Julia channels (default marked with *):\n");
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("st")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout("Installed Julia channels (default marked with *):\n");
 }
