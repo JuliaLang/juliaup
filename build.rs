1de7cc82b395f658908477ccf511f5ba87622fc3
anand jain <33790004+anandijain@users.noreply.github.com>
2022-05-31 19:08:39 -0700

    new rc

--- build.rs
+++ build.rs
@@ -65,6 +65,7 @@ fn produce_version_db() -> Result<JuliaupVersionDB> {
     original_available_versions.push(Version::parse("1.7.3")?);
     original_available_versions.push(Version::parse("1.8.0-beta1")?);
     original_available_versions.push(Version::parse("1.8.0-beta3")?);
+    original_available_versions.push(Version::parse("1.8.0-rc1")?);
 
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH")?;
     let target_os = std::env::var("CARGO_CFG_TARGET_OS")?;
