bdf51d3c0b9e47269320d95bbd0636a354bb59b6
David Anthoff <anthoff@berkeley.edu>
2022-07-17 20:50:10 -0700

    Remove diagnostic code

--- src/operations.rs
+++ src/operations.rs
@@ -44,7 +44,6 @@ where
 
 #[cfg(not(any(windows, macos)))]
 pub fn get_ureq_agent() -> Result<ureq::Agent> {
-    eprintln!("NOT USING NATIVE TLS");
     let agent = ureq::AgentBuilder::new().build();
 
     Ok(agent)
@@ -52,7 +51,6 @@ pub fn get_ureq_agent() -> Result<ureq::Agent> {
 
 #[cfg(any(windows, macos))]
 pub fn get_ureq_agent() -> Result<ureq::Agent> {
-    eprintln!("USING NATIVE TLS");
     use std::sync::Arc;
     use native_tls::TlsConnector;
 
