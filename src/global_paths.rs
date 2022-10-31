b7db95136503ea8d3621d95ceda2f1978eaf7841
Camillo Schenone <camillo.schenone@gmail.com>
2022-10-31 13:30:07 +0100

    Added JULIAUP_SELF_HOME support to the GlobalPath.

--- src/global_paths.rs
+++ src/global_paths.rs
@@ -1,12 +1,9 @@
 use std::path::PathBuf;
-use anyhow::{Result,bail,anyhow};
-#[cfg(feature = "selfupdate")]
-use anyhow::Context;
+use anyhow::{Result,bail,anyhow, Context};
 pub struct GlobalPaths {
     pub juliauphome: PathBuf,
     pub juliaupconfig: PathBuf,
     pub lockfile: PathBuf,
-    #[cfg(feature = "selfupdate")]
     pub juliaupselfhome: PathBuf,
     #[cfg(feature = "selfupdate")]
     pub juliaupselfconfig: PathBuf,
@@ -28,12 +25,12 @@ fn get_juliaup_home_path() -> Result<PathBuf> {
             path.join("juliaup")
         }
         Err(_) => {
-    let path = dirs::home_dir()
-        .ok_or(anyhow!(
-            "Could not determine the path of the user home directory."
-        ))?
-        .join(".julia")
-        .join("juliaup");
+            let path = dirs::home_dir()
+            .ok_or(anyhow!(
+                "Could not determine the path of the user home directory."
+            ))?
+            .join(".julia")
+            .join("juliaup");
 
             if !path.is_absolute() {
                 bail!(
@@ -49,29 +46,43 @@ fn get_juliaup_home_path() -> Result<PathBuf> {
     Ok(path)
 }
 
+fn get_juliaup_selfhome_path() -> Result<PathBuf> {
+    let entry_sep = if std::env::consts::OS == "windows" {';'} else {':'};
+
+    let selfpath = match std::env::var("JULIAUP_SELF_HOME") {
+        Ok(val) => {
+            let path = PathBuf::from(val.to_string().split(entry_sep).next().unwrap());
+            if !path.is_absolute() { bail!("The `JULIAUP_SELF_HOME` environment variable contains a value that resolves to an an invalid path `{}`.", path.display()); };
+
+            path
+        }
+        Err(_) => {
+            let my_own_path = std::env::current_exe()
+                .with_context(|| anyhow!("Could not determine the path of the running exe."))?;
+            my_own_path
+                .parent()
+                .ok_or_else(|| anyhow!("Failed to get path of folder of own executable."))?
+                .parent()
+                .ok_or_else(|| anyhow!("Failed to get parent path of folder of own executable."))?
+                .to_path_buf()
+        }
+    };
+
+    Ok(selfpath)
+}
+
 pub fn get_paths() -> Result<GlobalPaths> {
+
     let juliauphome = get_juliaup_home_path()?;
 
-    #[cfg(feature = "selfupdate")]
-    let my_own_path = std::env::current_exe()
-        .with_context(|| "Could not determine the path of the running exe.")?;
-
-    #[cfg(feature = "selfupdate")]
-    let juliaupselfbin = my_own_path.parent()
-        .ok_or_else(|| anyhow!("Could not determine parent."))?
-        .to_path_buf();
-
     let juliaupconfig = juliauphome.join("juliaup.json");
-    
+
     let lockfile = juliauphome.join(".juliaup-lock");
 
+    let juliaupselfhome = get_juliaup_selfhome_path()?;
+
     #[cfg(feature = "selfupdate")]
-    let juliaupselfhome = my_own_path
-        .parent()
-        .ok_or_else(|| anyhow!("Failed to get path of folder of own executable."))?
-        .parent()
-        .ok_or_else(|| anyhow!("Failed to get parent path of folder of own executable."))?
-        .to_path_buf();
+    let juliaupselfbin = juliaselfhome.join("bin");
 
     #[cfg(feature = "selfupdate")]
     let juliaupselfconfig = juliaupselfhome
@@ -81,7 +92,6 @@ pub fn get_paths() -> Result<GlobalPaths> {
         juliauphome,
         juliaupconfig,
         lockfile,
-        #[cfg(feature = "selfupdate")]
         juliaupselfhome,
         #[cfg(feature = "selfupdate")]
         juliaupselfconfig,
