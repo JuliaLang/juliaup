9446818f525535f3a48c5fa5cb2475638f2ec68e
David Anthoff <anthoff@berkeley.edu>
2022-11-22 16:34:14 -0800

    Only delete own files with self uninstall

--- src/command_selfuninstall.rs
+++ src/command_selfuninstall.rs
@@ -47,11 +47,53 @@ pub fn run_command_selfuninstall(paths: &crate::global_paths::GlobalPaths) -> Re
     };
 
     if paths.juliauphome != paths.juliaupselfhome {
-        eprint!("Deleting Juliaup folder {:?}.", paths.juliaupselfhome);
-        match std::fs::remove_dir_all(&paths.juliaupselfhome) {
+        let juliaup_binfolder_path = paths.juliaupselfhome.join("bin");
+        let julia_symlink_path = juliaup_binfolder_path.join("julia");
+        let julialauncher_path = juliaup_binfolder_path.join("julialauncher");
+        let juliaup_path = juliaup_binfolder_path.join("juliaup");
+        let juliaup_config_path = paths.juliaupselfhome.join("juliaupself.json");
+
+        eprint!("Deleting julia symlink {:?}.", julia_symlink_path);
+        match std::fs::remove_file(&julia_symlink_path) {
             Ok(_) => eprintln!(" Success."),
             Err(_) => eprintln!(" Failed.")
         };
+
+        eprint!("Deleting julialauncher binary {:?}.", julialauncher_path);
+        match std::fs::remove_file(&julialauncher_path) {
+            Ok(_) => eprintln!(" Success."),
+            Err(_) => eprintln!(" Failed.")
+        };
+
+        eprint!("Deleting juliaup binary {:?}.", juliaup_path);
+        match std::fs::remove_file(&juliaup_path) {
+            Ok(_) => eprintln!(" Success."),
+            Err(_) => eprintln!(" Failed.")
+        };
+
+        if juliaup_binfolder_path.read_dir()?.next().is_none() {
+            eprint!("Deleting the Juliaup bin folder {:?}.", juliaup_binfolder_path);
+            match std::fs::remove_dir(&juliaup_binfolder_path) {
+                Ok(_) => {
+                    eprintln!(" Success.");
+
+                    if paths.juliaupselfhome.read_dir()?.next().is_none() {
+                        eprint!("Deleting the Juliaup folder {:?}.", paths.juliaupselfhome);
+                        match std::fs::remove_dir(&paths.juliaupselfhome) {
+                            Ok(_) => eprintln!(" Success."),
+                            Err(_) => eprintln!(" Failed, skipping removal of the entire Juliaup folder.")
+                        };
+                    }
+                    else {
+                        eprintln!("The Juliaup folder {:?} is not empty, skipping removal of the entire Juliaup folder.", paths.juliaupselfhome);
+                    }
+                },
+                Err(_) => eprintln!(" Failed, skipping removal of the entire Juliaup folder.")
+            };
+        }
+        else {
+            eprintln!("The Juliaup bin folder {:?} is not empty, skipping removal of the entire Juliaup folder.", juliaup_binfolder_path);
+        }
     }
 
     eprintln!("Successfully removed Juliaup.");
