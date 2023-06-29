30d00a1f9b1a0a8d1e6513e9bfdcdfb0a1ba0e8b
David Anthoff <anthoff@berkeley.edu>
2023-06-28 17:11:08 -0700

    Progress on MSI installer

--- build_msi.ps1
+++ build_msi.ps1
@@ -1,1 +1,1 @@
-wix build -ext WixToolset.UI.wixext .\deploy\msi\Julia.wxs -b .\target\debug\ -arch x86 -o target\msi\Julia.msi
+wix build -ext WixToolset.UI.wixext .\deploy\msi\Julia.wxs -b .\target\release\ -arch x86 -o target\msi\Julia.msi
