85d1e268436bd2955513ffa77c958c575b433210
David Anthoff <anthoff@berkeley.edu>
2023-06-29 11:14:28 -0700

    Add custom bitmaps for MSI installer

--- build_msi.ps1
+++ build_msi.ps1
@@ -4,5 +4,6 @@ Copy-Item .\target\x86_64-pc-windows-gnu\release\*.exe .\target\msi\x86_64-pc-wi
 # Copy-Item .\target\i686-pc-windows-gnu\release\*.exe .\target\msi\i686-pc-windows-gnu\release
 # Copy-Item .\deploy\msi\License.rtf .\target\msi\i686-pc-windows-gnu\release
 Copy-Item .\deploy\msi\License.rtf .\target\msi\x86_64-pc-windows-gnu\release
+Copy-Item .\deploy\msi\Bitmaps .\target\msi\x86_64-pc-windows-gnu\release -Recurse -Force
 # wix build -ext WixToolset.UI.wixext .\deploy\msi\Julia.wxs -b .\target\msi\i686-pc-windows-gnu\release -arch x86 -o target\msi\Julia-x86.msi
 wix build -ext WixToolset.UI.wixext .\deploy\msi\Julia.wxs -b .\target\msi\x86_64-pc-windows-gnu\release -arch x64 -o target\msi\Julia-x64.msi
