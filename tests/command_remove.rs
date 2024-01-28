b2239e19800a87fb855597ea3998c3f86115702d
David Anthoff <anthoff@berkeley.edu>
2024-01-28 13:04:50 -0800

    Add some basic tests

--- tests/command_remove.rs
+++ tests/command_remove.rs
@@ -73,4 +73,42 @@ fn command_remove() {
         .assert()
         .success()
         .stdout(predicates::str::contains("1.6.4").and(predicates::str::contains("release").not()));
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("add")
+        .arg("nightly")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .env("JULIAUP_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout("");
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("status")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .env("JULIAUP_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("1.6.4").and(predicates::str::contains("-DEV")));
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("remove")
+        .arg("nightly")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .env("JULIAUP_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout("");
+
+    Command::cargo_bin("juliaup")
+        .unwrap()
+        .arg("status")
+        .env("JULIA_DEPOT_PATH", depot_dir.path())
+        .env("JULIAUP_DEPOT_PATH", depot_dir.path())
+        .assert()
+        .success()
+        .stdout(predicates::str::contains("1.6.4").and(predicates::str::contains("-DEV").not()));
 }
