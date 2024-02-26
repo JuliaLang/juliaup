56056bc1c2115f69f64c64c29c064e71b4d5f79e
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2024-02-26 16:10:39 -0400

    Add is_valid_channel and use it everywhere `version_db.available_channels` is used

--- src/command_link.rs
+++ src/command_link.rs
@@ -3,6 +3,7 @@ use crate::config_file::{load_mut_config_db, save_config_db};
 use crate::global_paths::GlobalPaths;
 #[cfg(not(windows))]
 use crate::operations::create_symlink;
+use crate::operations::is_valid_channel;
 use crate::versions_file::load_versions_db;
 use anyhow::{bail, Context, Result};
 use path_absolutize::Absolutize;
@@ -24,7 +25,7 @@ pub fn run_command_link(
         bail!("Channel name `{}` is already used.", channel)
     }
 
-    if versiondb_data.available_channels.contains_key(channel) {
+    if is_valid_channel(&versiondb_data, &channel.to_string()) {
         eprintln!("WARNING: The channel name `{}` is also a system channel. By linking your custom binary to this channel you are hiding this system channel.", channel);
     }
 
