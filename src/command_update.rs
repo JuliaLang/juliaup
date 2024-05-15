86d80bde7530eaadc8ea6a753e95d51d4f4f1db6
Camillo Schenone <camillo.schenone@gmail.com>
2024-05-15 17:43:46 +0200

    fix: better message

--- src/command_update.rs
+++ src/command_update.rs
@@ -26,7 +26,7 @@ fn update_channel(
 
             if let Some(should_version) = should_version {
                 if &should_version.version != version {
-                    eprintln!("Updating channel: {}", channel);
+                    eprintln!("Channel: {} - Updating", channel);
 
                     install_version(&should_version.version, config_db, version_db, paths)
                         .with_context(|| {
