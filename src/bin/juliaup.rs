487753fc4e7f4efc376a8f6d06624c37d177dfec
Ian Butterworth <i.r.butterworth@gmail.com>
2025-08-26 18:08:26 -0400

    Update src/bin/juliaup.rs

--- src/bin/juliaup.rs
+++ src/bin/juliaup.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use juliaup::cli::{ConfigSubCmd, Juliaup, OverrideSubCmd, SelfSubCmd};
 use juliaup::command_api::run_command_api;
 use juliaup::command_completions::generate_completion_for_command;
-use juliaup::command_completions::run_command_completions;
 use juliaup::command_config_autoinstall::run_command_config_autoinstall;
 #[cfg(not(windows))]
 use juliaup::command_config_symlinks::run_command_config_symlinks;
