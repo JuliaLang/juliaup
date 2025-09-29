84b37e5402332cf1c3d419722a7d41bc9fd6f677
Ian Butterworth <i.r.butterworth@gmail.com>
2025-09-29 15:55:17 -0400

    fix overeager clippy warnings

--- src/jsonstructs_versionsdb.rs
+++ src/jsonstructs_versionsdb.rs
@@ -2,18 +2,21 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
+#[allow(dead_code)]
 pub struct JuliaupVersionDBVersion {
     #[serde(rename = "UrlPath")]
     pub url_path: String,
 }
 
 #[derive(Serialize, Deserialize)]
+#[allow(dead_code)]
 pub struct JuliaupVersionDBChannel {
     #[serde(rename = "Version")]
     pub version: String,
 }
 
 #[derive(Serialize, Deserialize)]
+#[allow(dead_code)]
 pub struct JuliaupVersionDB {
     #[serde(rename = "AvailableVersions")]
     pub available_versions: HashMap<String, JuliaupVersionDBVersion>,
