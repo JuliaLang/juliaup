531e516cfeba04a5f483860c5fcf3a62aecf6ca3
Ian Butterworth <i.r.butterworth@gmail.com>
2025-10-07 10:47:14 -0400

    slight revert

--- src/bin/julialauncher.rs
+++ src/bin/julialauncher.rs
@@ -486,7 +486,7 @@ fn get_julia_path_from_installed_channel(
             server_etag,
             version: _,
         } => {
-            if is_interactive() && local_etag != server_etag {
+            if local_etag != server_etag && is_interactive() {
                 if channel.starts_with("nightly") {
                     // Nightly is updateable several times per day so this message will show
                     // more often than not unless folks update a couple of times a day.
