320a1b6de65b8a42f485f6e1ee942e74e2d66c12
Kristoffer Carlsson <kcarlsson89@gmail.com>
2025-10-31 19:52:07 +0100

    match against environment manifest version before using the default channel
    alternative to JuliaLang#1095

--- tests/channel_selection.rs
+++ tests/channel_selection.rs
@@ -184,3 +184,59 @@ fn auto_install_valid_channel() {
             "Info: Installing Julia 1.10.10 automatically per juliaup settings...",
         ));
 }
+
+fn write_project(project_dir: &std::path::PathBuf, manifest_version: &str, compat: Option<&str>) {
+    std::fs::create_dir_all(project_dir).unwrap();
+
+    std::fs::write(
+        project_dir.join("Project.toml"),
+        format!(
+            r#"
+name = "AutoProject"
+uuid = "00000000-0000-0000-0000-000000000001"
+version = "0.1.0"
+
+{}
+"#,
+            compat
+                .map(|c| format!("[compat]\njulia = \"{}\"", c))
+                .unwrap_or_default()
+        ),
+    )
+    .unwrap();
+
+    std::fs::write(
+        project_dir.join("Manifest.toml"),
+        format!(
+            r#"
+julia_version = "{}"
+"#,
+            manifest_version
+        ),
+    )
+    .unwrap();
+}
+
+fn install_channel(env: &TestEnv, channel: &str) {
+    env.juliaup().arg("add").arg(channel).assert().success();
+}
+
+#[test]
+fn end_to_end_manifest_selection() {
+    let env = TestEnv::new();
+    install_channel(&env, "1.8.2");
+
+    let project_dir = env.depot_path().join("manifest_project");
+    write_project(&project_dir, "1.8.2", None);
+
+    env.julia()
+        .arg(format!(
+            "--project={}",
+            project_dir.as_os_str().to_string_lossy()
+        ))
+        .arg("-e")
+        .arg("print(VERSION)")
+        .assert()
+        .success()
+        .stdout("1.8.2");
+}
