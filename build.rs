44ee3f7d6d3cda53aaa24a112f3c1bd76273324f
Ian Butterworth <i.r.butterworth@gmail.com>
2025-11-02 21:23:57 -0500

    try to use dmgs first on macos

--- build.rs
+++ build.rs
@@ -19,7 +19,7 @@ fn main() -> Result<()> {
 
     let db_path = Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap())
         .join("versiondb")
-        .join(format!("versiondb-{}.json", target_platform));
+        .join(format!("versiondb-{target_platform}.json"));
 
     let version_db_path = out_path.join("versionsdb.json");
     std::fs::copy(&db_path, &version_db_path).unwrap();
@@ -33,8 +33,7 @@ fn main() -> Result<()> {
     std::fs::write(
         &bundled_version_path,
         format!(
-            "pub const BUNDLED_JULIA_VERSION: &str = {}; pub const BUNDLED_DB_VERSION: &str = {};",
-            bundled_version_as_string, bundled_dbversion_as_string
+            "pub const BUNDLED_JULIA_VERSION: &str = {bundled_version_as_string}; pub const BUNDLED_DB_VERSION: &str = {bundled_dbversion_as_string};"
         ),
     )
     .unwrap();
